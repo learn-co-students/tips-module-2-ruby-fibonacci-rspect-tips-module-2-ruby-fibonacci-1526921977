1cf7cf090f7f06ca07727f007a733803e73692ab
sgg2123 <sgg2123@columbia.edu>
2018-05-21 14:58:46 -0400

    Done

--- fibonacci/fibonacci.rb
+++ fibonacci/fibonacci.rb
@@ -3,7 +3,15 @@ require 'pry-byebug'
 # but you may remove it if you like.
 
 def fibonacci(n)
-  # your code here :)
+  if n.class != Fixnum || n < 0
+    return "Please enter a positive integer."
+  elsif n == 0
+    0
+  elsif n == 1
+    1
+  else
+    fibonacci(n-2) + fibonacci(n-1)
+  end
 end
 
 Pry.start
